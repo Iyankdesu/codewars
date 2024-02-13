4d549d614d0120be63a3a6b062be7965803ec114
Muhammad Fajrian Nur Ibrahim <iyankdesu@gmail.com>
2024-02-14 02:43:20 +0800

    feat: add nesting_structure_comparison.py solution

---
+++ src/nesting_structure_comparison.py
@@ -0,0 +1,48 @@
+# Nesting Structure Comparison
+
+"""
+Complete the function/method (depending on the language) to return true/True when its argument is an array that has the same nesting structures and same corresponding length of nested arrays as the first array.
+
+For example:
+
+# should return True
+same_structure_as([ 1, 1, 1 ], [ 2, 2, 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ 2, [ 2, 2 ] ] )
+
+# should return False 
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2, 2 ], 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2 ], 2 ] )
+
+# should return True
+same_structure_as([ [ [ ], [ ] ] ], [ [ [ ], [ ] ] ] )
+
+# should return False
+same_structure_as([ [ [ ], [ ] ] ], [ [ 1, 1 ] ] )
+"""
+
+def same_structure_as(original,other):
+    if type(original) != list or type(other) != list or len(original) != len(other) or type(original) != type(other):
+        return False
+    for ori, oth in zip(original, other):
+        if type(ori) != type(oth) and ori not in other:
+            return False
+        if type(ori) is list and type(oth) is list:
+            return same_structure_as(ori, oth)
+    return True
+
+"""
+def same_structure_as(original,other):
+    if isinstance(original, list) and isinstance(other, list) and len(original) == len(other):
+        for o1, o2 in zip(original, other):
+            if not same_structure_as(o1, o2): return False
+        else: return True
+    else: return not isinstance(original, list) and not isinstance(other, list)
+"""
+
+"""
+def same_structure_as(original, other):
+    if type(original) == list == type(other):
+        return len(original) == len(other) and all(map(same_structure_as, original, other))
+    else:
+        return type(original) != list != type(other)
+"""
